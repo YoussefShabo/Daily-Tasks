346149bdd3d5ebe559cc1b5450de6a6871564fd3
YoussefShabo <youssefshabo@gmail.com>
2023-07-05 14:56:07 +0200

    July5th

--- 07-05/python.py
+++ 07-05/python.py
@@ -1,3 +1,22 @@
-print("*" * 15)
-print("Hello Earth!👾")
-print("*" * 15)
+STUDENT_COUNT = 1000
+rating = 4.99
+is_published = True
+course_name = "Python Programming"
+# print(len(course_name))
+# print(course_name[0])
+# print(course_name[-1])
+# print(course_name[0:])
+# print(course_name[:])
+
+
+first = "Youssef"
+last = "Shabo"
+full = f"{first} {last}"
+# print(full)
+
+
+course = "   Python Programming"
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.strip())
