5d2ed71db392dfc356dfee266dfa43e780fef7ca
YoussefShabo <youssefshabo@gmail.com>
2023-07-11 23:15:11 +0200

    July 11th

--- 07-11/app.js
+++ 07-11/app.js
@@ -15,3 +15,23 @@
 // 1050 -> 105
 // -1050 -> -105
 // Zero alone is fine, don't worry about it. Poor guy anyway
+
+function noBoringZeros(n) {
+  // Exclude removing the 0 if it was alone.
+  if (n === 0) {
+    return n;
+  }
+
+  // Convert the number to a string
+  let numberString = n.toString();
+
+  // Remove trailing zeros
+  while (numberString.endsWith("0")) {
+    numberString = numberString.slice(0, -1);
+  }
+
+  // Convert the modified string back to a number
+  const result = parseFloat(numberString);
+
+  return result;
+}
