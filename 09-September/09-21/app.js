65908fcf787c1b71a8281906cdd0a0188ef4bcb4
Youssef Shabo <youssefshabo@gmail.com>
2023-09-22 12:16:19 -0500

    sept 22nd

--- 09-September/09-21/app.js
+++ 09-September/09-21/app.js
@@ -2,11 +2,8 @@
 const movingDivs = document.querySelectorAll('.movingDiv');
 
 function moveRandomly(element) {
-    const maxX = window.innerWidth - element.clientWidth;
-    const maxY = window.innerHeight - element.clientHeight;
-    
-    const newX = Math.random() * maxX;
-    const newY = Math.random() * maxY;
+    const newX = Math.random() * window.innerWidth;
+    const newY = Math.random() * window.innerHeight;
 
     element.style.left = `${newX}px`;
     element.style.top = `${newY}px`;
@@ -18,4 +15,4 @@ movingDivs.forEach(moveRandomly);
 // Move the divs periodically (e.g., every 2 seconds)
 setInterval(() => {
     movingDivs.forEach(moveRandomly);
-}, 2000);
+}, 4000);
