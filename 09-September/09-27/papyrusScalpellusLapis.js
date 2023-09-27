a2a188a5469b03cf023cb0384035911b9bdbefef
Youssef Shabo <youssefshabo@gmail.com>
2023-09-27 18:44:21 -0500

    sept 27th

--- 09-September/09-27/papyrusScalpellusLapis.js
+++ 09-September/09-27/papyrusScalpellusLapis.js
@@ -1,14 +1,2 @@
-let i = 0;
-        while (i < 100) {
-            i++;
-
-            if (i % 3 == 0 && i % 5 == 0) {
-                console.log(i + ". FizzBuzz<br/>");
-            } else if (i % 3 == 0) {
-                console.log(i + ". Fizz<br/>")
-            } else if (i % 5 == 0) {
-                console.log(i + ". Buzz<br/>")
-            } else {
-                console.log(i + ". <br/>")
-            }
-        }
+let playerChoice = null;
+let computerChoice = null;
