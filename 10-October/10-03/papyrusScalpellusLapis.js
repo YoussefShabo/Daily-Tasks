8b1a7dbde3d636b0d295a80bc7b12fb8533ae48e
YoussefShabo <youssefshabo@gmail.com>
2023-10-22 17:34:15 -0500

    oct22nd

--- 10-October/10-03/papyrusScalpellusLapis.js
+++ 10-October/10-03/papyrusScalpellusLapis.js
@@ -1,2 +1,10 @@
-let playerChoice = null;
-let computerChoice = null;
+function cookingTime(eggs) {
+  // TODO
+  if (eggs > 0) {
+    const time = Math.ceil(eggs / 8) * 5;
+    console.log(time);
+  }
+}
+
+let eggs = 9;
+cookingTime(eggs);
